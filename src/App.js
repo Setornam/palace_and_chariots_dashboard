fa74b3798eeea71ed161d7c1ac7808aa8bae9429
Setor Kwame <setorkg@gmail.com>
2023-09-07 11:01:52 +0000

    Login Protected routes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import SplashScreen from './components/auth/SplashScreen';
 import Signup from './components/auth/Signup';
 import Login from './components/auth/Login';
 import PasswordReset from './components/auth/PasswordReset';
 import Admin from './components/Admin';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './components/auth/firebase';
 
-
- 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     setTimeout(() => {
       setLoading(false);
     }, 2000); // 2 seconds
 
+    const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+      if (authUser) {
+        setUser(authUser);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
   }, []);
- 
+
+  // A custom function to check if the user is authenticated
+  const isAuthenticated = () => !!user;
+
   return (
     <Router>
-                                   
-            <Routes>                                                                        
-              <Route path="/" element={loading ? <SplashScreen/> : <Login />}/>
-               <Route path="/signup" element={<Signup/>}/>
+      <Routes>
+        <Route path="/" element={loading ? <SplashScreen /> : <Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/password-reset" element={<PasswordReset />} />
 
-               <Route path="/password-reset" element={<PasswordReset/>}/>
-               <Route path= "/admin" element={<Admin/>}/>
-            </Routes>                    
-        
+        {/* Protected route */}
+        <Route
+          path="/admin"
+          element={isAuthenticated() ? <Admin /> : <Navigate to="/" />}
+        />
+      </Routes>
     </Router>
   );
-}
- 
+};
+
 export default App;
